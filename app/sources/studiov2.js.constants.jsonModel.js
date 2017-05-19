7a8e869241b5af04a50761635860db6bb42df513
Danilo Rodrigues <danilomelorodrigues@gmail.com>
2017-05-19 16:52:02 -0300

    fix na forma de assinar a constant

--- app/sources/studiov2.js.constants.jsonModel.js
+++ app/sources/studiov2.js.constants.jsonModel.js
@@ -1,72 +1,70 @@
 (function(){
+
   angular
     .module('studio-v2')
-    .constant('JSONMODEL', jsonModel);
-
-  var jsonModel = {
-    "key": "", 
-    "label": "",
-    "pagination": {
-      "type": "server",
-      "countPerPage": 10 
-    },
-    "dataSource": {},
-    "views": {
-      "list": {
-        "actions":[
-          {
-            "action": "new",
-            "name": "new",
-            "label": "button.new.title"
-          },
-          {
-            "action": "list.view_edit", 
-            "name": "view_edit",
-            "label": "button.viewedit.title",
-            "notDisplayLabel": true
-          },
-          {
-            "action": "list.remove",
-            "name": "listDelete",
-            "label": "button.remove.title",
-            "notDisplayLabel": true
-          }
-        ],
-        "breadcrumb": []
+    .constant('JSONMODEL', {
+      "key": "", 
+      "label": "",
+      "pagination": {
+        "type": "server",
+        "countPerPage": 10 
       },
-      "edit": {
-        "actions": [
-          {
-            "action": "cancel",
-            "label": "button.cancel.title",
-            "name": "cancel"
-          },
-          {
-            "action": "remove",
-            "label": "button.remove.title",
-            "name": "remove"
-          },
-          {
-            "action": "duplicate",
-            "label": "button.duplicate.title",
-            "name": "duplicate",
-          },
-          {
-            "action": "savenew",
-            "label": "button.savenew.title",
-            "name": "save"
-          },
-          {
-            "action": "save",
-            "label": "button.save.title",
-            "name": "save"
-          }
-        ],
-        "breadcrumb": []
+      "dataSource": {},
+      "views": {
+        "list": {
+          "actions":[
+            {
+              "action": "new",
+              "name": "new",
+              "label": "button.new.title"
+            },
+            {
+              "action": "list.view_edit", 
+              "name": "view_edit",
+              "label": "button.viewedit.title",
+              "notDisplayLabel": true
+            },
+            {
+              "action": "list.remove",
+              "name": "listDelete",
+              "label": "button.remove.title",
+              "notDisplayLabel": true
+            }
+          ],
+          "breadcrumb": []
+        },
+        "edit": {
+          "actions": [
+            {
+              "action": "cancel",
+              "label": "button.cancel.title",
+              "name": "cancel"
+            },
+            {
+              "action": "remove",
+              "label": "button.remove.title",
+              "name": "remove"
+            },
+            {
+              "action": "duplicate",
+              "label": "button.duplicate.title",
+              "name": "duplicate",
+            },
+            {
+              "action": "savenew",
+              "label": "button.savenew.title",
+              "name": "save"
+            },
+            {
+              "action": "save",
+              "label": "button.save.title",
+              "name": "save"
+            }
+          ],
+          "breadcrumb": []
+        },
+        "filter":{}
       },
-      "filter":{}
-    },
-    "fields": []
-  }
-
+      "fields": []
+    });
 })();
