c6cb890e27e7db089b18801dccb60766250b2f04
Danilo Rodrigues <danilomelorodrigues@gmail.com>
2017-07-19 16:08:53 -0300

    reindent

--- app/sources/studiov2.js.app.js
+++ app/sources/studiov2.js.app.js
@@ -9,79 +9,79 @@
       .config(config)
       .run(run);
 
-      function config(NotificationProvider, $stateProvider, $urlRouterProvider, $interpolateProvider) {
-        $interpolateProvider.startSymbol('{[{');
-        $interpolateProvider.endSymbol('}]}');
+    function config(NotificationProvider, $stateProvider, $urlRouterProvider, $interpolateProvider) {
+      $interpolateProvider.startSymbol('{[{');
+      $interpolateProvider.endSymbol('}]}');
 
-        $stateProvider
-          .state('forms', {
-            'url': '/forms',
-            'controller': 'FormEditController',
-            'controllerAs': 'ctrl',
-            'templateUrl': '/forms/studiov2.forms.dashboard'
-          })
-          .state('forms.new-view-edit', {
-            'url': '/new/view-edit',
-            'views':{
-              'sidebar': {
-                'templateUrl': '/forms/studiov2.forms.sidebar.edit'
-              },
-              'view': { 
-                'templateUrl': '/forms/studiov2.forms.view.edit'
-              }
+      $stateProvider
+        .state('forms', {
+          'url': '/forms',
+          'controller': 'FormEditController',
+          'controllerAs': 'ctrl',
+          'templateUrl': '/forms/studiov2.forms.dashboard'
+        })
+        .state('forms.new-view-edit', {
+          'url': '/new/view-edit',
+          'views':{
+            'sidebar': {
+              'templateUrl': '/forms/studiov2.forms.sidebar.edit'
+            },
+            'view': { 
+              'templateUrl': '/forms/studiov2.forms.view.edit'
             }
-          })
-          .state('forms.new-view-list', {
-            'url': '/new/view-list',
-            'views':{
-              'sidebar': {
-                'templateUrl': '/forms/studiov2.forms.sidebar.edit'
-              },
-              'view': {
-                'templateUrl': '/forms/studiov2.forms.view.list'
-              }
+          }
+        })
+        .state('forms.new-view-list', {
+          'url': '/new/view-list',
+          'views':{
+            'sidebar': {
+              'templateUrl': '/forms/studiov2.forms.sidebar.edit'
+            },
+            'view': {
+              'templateUrl': '/forms/studiov2.forms.view.list'
             }
-          })
-          .state('forms.edit-view-edit', {
-            'url': '/:id/view-edit',
-            'views':{
-              'sidebar': {
-                'templateUrl': '/forms/studiov2.forms.sidebar.edit'
-              },
-              'view': {
-                'templateUrl': '/forms/studiov2.forms.view.edit'
-              }
+          }
+        })
+        .state('forms.edit-view-edit', {
+          'url': '/:id/view-edit',
+          'views':{
+            'sidebar': {
+              'templateUrl': '/forms/studiov2.forms.sidebar.edit'
+            },
+            'view': {
+              'templateUrl': '/forms/studiov2.forms.view.edit'
             }
-          })
-          .state('forms.edit-view-list', {
-            'url': '/:id/view-list',
-            'views':{
-              'sidebar': {
-                'templateUrl': '/forms/studiov2.forms.sidebar.edit'
-              },
-              'view': {
-                'templateUrl': '/forms/studiov2.forms.view.list'
-              }
+          }
+        })
+        .state('forms.edit-view-list', {
+          'url': '/:id/view-list',
+          'views':{
+            'sidebar': {
+              'templateUrl': '/forms/studiov2.forms.sidebar.edit'
+            },
+            'view': {
+              'templateUrl': '/forms/studiov2.forms.view.list'
             }
-          });
-
-
-        // $locationProvider.html5Mode(true);
-        
-        NotificationProvider.setOptions({
-          delay: 4000,
-          startTop: 20,
-          startRight: 10,
-          verticalSpacing: 20,
-          horizontalSpacing: 20,
-          positionX: 'right',
-          positionY: 'top'
+          }
         });
-      }
 
-      function run($locale, $filter) {
-        $locale.NUMBER_FORMATS.DECIMAL_SEP = $filter('translate')('fieldformat.numeral.decimal_separator');
-        $locale.NUMBER_FORMATS.GROUP_SEP = $filter('translate')('fieldformat.numeral.grouping_separator');
-        $locale.NUMBER_FORMATS.CURRENCY_SYM = $filter('translate')('fieldformat.currency.symbol'); 
-      }
+
+      // $locationProvider.html5Mode(true);
+      
+      NotificationProvider.setOptions({
+        delay: 4000,
+        startTop: 20,
+        startRight: 10,
+        verticalSpacing: 20,
+        horizontalSpacing: 20,
+        positionX: 'right',
+        positionY: 'top'
+      });
+    }
+
+    function run($locale, $filter) {
+      $locale.NUMBER_FORMATS.DECIMAL_SEP = $filter('translate')('fieldformat.numeral.decimal_separator');
+      $locale.NUMBER_FORMATS.GROUP_SEP = $filter('translate')('fieldformat.numeral.grouping_separator');
+      $locale.NUMBER_FORMATS.CURRENCY_SYM = $filter('translate')('fieldformat.currency.symbol'); 
+    }
 })();
