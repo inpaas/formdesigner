f21e742502bb1b976e9a93031eb43b1d03a8c08e
Danilo Rodrigues <danilomelorodrigues@gmail.com>
2017-09-19 10:14:55 -0300

    fix format fullhour

--- app/constants/studiov2.js.constants.timeFormatPatterns.js
+++ app/constants/studiov2.js.constants.timeFormatPatterns.js
@@ -4,7 +4,7 @@
     .constant('TIME_FORMAT_PATTERNS', [
       {key: 'fieldformat.hour', title: 'HH', mask: '99'},
       {key: 'fieldformat.time', title: 'HH:mm', mask: '99:99'},
-      {key: 'fieldformat.full.hour', title: 'HH:mm:ss', mask: '99:99:99'},
+      {key: 'fieldformat.fullhour', title: 'HH:mm:ss', mask: '99:99:99'},
       {key: 'fieldformat.minutes', title: 'mm', mask: '99'}
     ]);
 })();
