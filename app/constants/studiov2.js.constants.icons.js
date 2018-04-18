ebf09f606282bcc6fefc7bf0ba62faecde7fbe60
Danilo Rodrigues <danilomelorodrigues@gmail.com>
2018-04-18 14:05:27 -0300

    tira o a IIFE desse arquivo

--- app/constants/studiov2.js.constants.icons.js
+++ app/constants/studiov2.js.constants.icons.js
@@ -1,12 +1,10 @@
-(function(){
-  angular
-    .module('studio-v2')
-    .constant('ICONS', [
-      {class: 'fa-pencil'},
-      {class: 'fa-user-o'},
-      {class: 'fa-question'},
-      {class: 'fa-info'},
-      {class: 'fa-address-book'},
-      {class: 'fa-play'}
-    ]);
-})();
+angular
+  .module('studio-v2')
+  .constant('ICONS', [
+    {class: 'fa-pencil'},
+    {class: 'fa-user-o'},
+    {class: 'fa-question'},
+    {class: 'fa-info'},
+    {class: 'fa-address-book'},
+    {class: 'fa-play'}
+  ]);
