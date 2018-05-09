35f1af242a47ce70225fc7b4381394b8c7388f38
Danilo Rodrigues <danilomelorodrigues@gmail.com>
2018-05-09 16:43:52 -0300

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "warn",
+            2
+        ],
+        "linebreak-style": [
+            "off",
+            "unix"
+        ],
+        "quotes": [
+            "warn",
+            "single"
+        ],
+        "semi": [
+            "warn",
+            "always"
+        ]
+    }
+};
